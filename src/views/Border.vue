da5a2722d5d8a103b970282f4e5bdb6c395fee43
yunser <>
2019-10-02 13:11:40 +0800

    连接到 CSS 三角形

---
+++ src/views/Border.vue
@@ -0,0 +1,50 @@
+<template>
+    <my-page title="CSS 边框生成">
+        <div class="common-container container">
+            <div class="preview" :style="previewStyle"></div>
+            <div class="form">
+                <ui-slider class="slider" v-model="width" :min="1" :max="100" :step="1"/>
+            </div>
+            <pre><code>{{ code }}</code></pre>
+        </div>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                width: 1
+            }
+        },
+        computed: {
+            previewStyle() {
+                return {
+                    border: `${this.width}px solid #000`
+                }
+            },
+            code() {
+                return `.demo {
+    border: ${this.width}px solid #000;
+}`
+            }
+        },
+        mounted() {
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+    .preview {
+        width: 100px;
+        height: 100px;
+        margin-bottom: 24px;
+        border: 1px solid #000;
+    }
+    .form {
+        width: 240px;
+        margin-bottom: 24px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,67 @@
 <template>
-    <my-page title="CSS 生成工具">
-        <div class="preview" :style="previewStyle"></div>
-        <div class="form">
-            <ui-slider class="slider" v-model="width" :min="1" :max="100" :step="1"/>
+    <my-page title="CSS" :page="page">
+        <div class="common-container">
+            <app-list :data="groups" />
         </div>
-        <pre><code>{{ code }}</code></pre>
     </my-page>
 </template>
 
 <script>
+    /* eslint-disable */
     export default {
         data () {
             return {
-                width: 1
+                groups: [
+                    {
+                        apps: [
+                            {
+                                name: 'CSS 三角形',
+                                desc: '',
+                                icon: 'https://icons.yunser.com/icons/tool.svg',
+                                to: 'xxx',
+                                href: 'https://csstriangle.yunser.com/',
+                                target: '_blank'
+                            },
+                            {
+                                name: 'CSS 边框生成',
+                                desc: '',
+                                icon: 'https://icons.yunser.com/icons/tool.svg',
+                                to: '/border'
+                            }
+                        ]
+                    },
+                ],
+                page: {
+                    menu: [
+                        {
+                            type: 'icon',
+                            icon: 'search',
+                            href: 'https://search.yunser.com?utm_source=edu',
+                            target: '_blank',
+                            title: '搜索'
+                        },
+                        {
+                            type: 'icon',
+                            icon: 'apps',
+                            href: 'https://app.yunser.com?utm_source=edu',
+                            target: '_blank',
+                            title: '应用'
+                        }
+                    ]
+                }
             }
         },
         computed: {
-            previewStyle() {
-                return {
-                    border: `${this.width}px solid #000`
-                }
-            },
-            code() {
-                return `.demo {
-    border: ${this.width}px solid #000;
-}`
-            }
         },
         mounted() {
         },
         methods: {
+            init() {
+            },
+            fileChange(e) {
+            },
+            sizeStr: function (size) {
+            }
         }
     }
 </script>
-
-<style scoped>
-    .preview {
-        width: 100px;
-        height: 100px;
-        margin-bottom: 24px;
-        border: 1px solid #000;
-    }
-    .form {
-        width: 240px;
-        margin-bottom: 24px;
-    }
-</style>
